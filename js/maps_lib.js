99469857ca3e42d7d8c1d0dd5301721cab323e82
fairfaxdup <Fairfax@MacBook-Pro-4.local>
2016-01-20 14:17:25 -0600

    changelist

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -348,6 +348,19 @@
  },
  
  
+    MapsLib.prototype.addCommas = function (nStr) {
+        nStr += '';
+        x = nStr.split('.');
+        x1 = x[0];
+        x2 = x.length > 1 ? '.' + x[1] : '';
+        var rgx = /(\d+)(\d{3})/;
+        while (rgx.test(x1)) {
+            x1 = x1.replace(rgx, '$1' + ',' + '$2');
+        }
+        return x1 + x2;
+    };
+ 
+ 
  
  
  MapsLib.prototype.getList = function(whereClause) {
@@ -395,21 +408,9 @@
  results.fadeIn();
  },
  
- 
- 
- 
 
-    MapsLib.prototype.addCommas = function (nStr) {
-        nStr += '';
-        x = nStr.split('.');
-        x1 = x[0];
-        x2 = x.length > 1 ? '.' + x[1] : '';
-        var rgx = /(\d+)(\d{3})/;
-        while (rgx.test(x1)) {
-            x1 = x1.replace(rgx, '$1' + ',' + '$2');
-        }
-        return x1 + x2;
-    };
+ 
+ 
 
     // maintains map centerpoint for responsive design
     MapsLib.prototype.calculateCenter = function () {
