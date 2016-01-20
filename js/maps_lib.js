bc710a8d152ce1af69bd0fa0bcff0ce71c545692
fairfaxdup <Fairfax@MacBook-Pro-4.local>
2016-01-20 14:34:55 -0600

    result

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -9,11 +9,11 @@
         this.searchRadius = options.searchRadius || 805; //in meters ~ 1/2 mile
 
         // the encrypted Table ID of your Fusion Table (found under File => About)
- this.fusionTableId = options.fusionTableId || "1kJw_x0AM88pHhtqYG9hw25HM_m6QUK6jgGmusT4p",
+ this.fusionTableId = options.fusionTableId || "",
 
         // Found at https://console.developers.google.com/
         // Important! this key is for demonstration purposes. please register your own.
-        this.googleApiKey = options.googleApiKey || "AIzaSyC6R2TN6YSoQ9SFe30vEVyc1pBVK0z6yTc",
+        this.googleApiKey = options.googleApiKey || "",
         
         // name of the location column in your Fusion Table.
         // NOTE: if your location column name has spaces in it, surround it with single quotes
